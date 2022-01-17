18c40971c959ed89b84ef4f10d20f2195e0dd46d
걷는사람 정정훈 <92363576+walkerJung@users.noreply.github.com>
2022-01-17 11:34:40 +0900

    2022-01-17 11:34

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -80,6 +80,7 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
                 originalTitle={movie.original_title}
                 voteAverage={movie.vote_average}
                 overview={movie.overview}
+                fullData={movie}
               />
             ))}
           </Swiper>
@@ -98,6 +99,7 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
           originalTitle={item.original_title}
           overview={item.overview}
           releaseDate={item.release_date}
+          fullData={item}
         />
       )}
     />
